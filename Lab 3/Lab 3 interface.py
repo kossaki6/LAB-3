83a04ccb48872d257a7df88af6cab9528e0ce5b1
hahaslav <yara-2004@ukr.net>
2024-10-17 13:46:29 +0300

    Add mode buttons

--- Lab 3/Lab 3 interface.py
+++ Lab 3/Lab 3 interface.py
@@ -9,57 +9,74 @@ class EquationApp:
         self.root = root
         self.root.title("Формування умови")
 
+        tk.Label(root, text="Режими роботи:").grid(row=0, column=0)
+        self.solve_mode_button = tk.Button(root, text="Розв'язування", command=self.solve_mode)
+        self.solve_mode_button.grid(row=0, column=1)
+        self.solve_mode_button = tk.Button(root, text="Формування умови", command=self.problem_generation_mode)
+        self.solve_mode_button.grid(row=0, column=2)
+
         # Введення для обмежень x1, x2
-        tk.Label(root, text="Обмеження для x1:").grid(row=0, column=0)
+        tk.Label(root, text="Обмеження для x1:").grid(row=1, column=0)
         self.x1_constraints_entry = tk.Entry(root)
-        self.x1_constraints_entry.grid(row=0, column=1)
+        self.x1_constraints_entry.grid(row=1, column=1)
 
-        tk.Label(root, text="Обмеження для x2:").grid(row=1, column=0)
+        tk.Label(root, text="Обмеження для x2:").grid(row=2, column=0)
         self.x2_constraints_entry = tk.Entry(root)
-        self.x2_constraints_entry.grid(row=1, column=1)
+        self.x2_constraints_entry.grid(row=2, column=1)
 
-        tk.Label(root, text="T:").grid(row=2, column=0)
+        tk.Label(root, text="T:").grid(row=3, column=0)
         self.t_entry = tk.Entry(root)
-        self.t_entry.grid(row=2, column=1)
+        self.t_entry.grid(row=3, column=1)
 
-        tk.Label(root, text="y(s):").grid(row=3, column=0)
+        self.y_label = tk.Label(root, text="y(s):")
+        self.y_label.grid(row=4, column=0)
         self.y_entry = tk.Entry(root)
-        self.y_entry.grid(row=3, column=1)
+        self.y_entry.grid(row=4, column=1)
 
         # Кнопка обчислення виразу для u(s)
         self.calculate_button = tk.Button(root, text="Обчислити u(s)", command=self.calculate_u)
-        self.calculate_button.grid(row=3, column=2)
+        self.calculate_button.grid(row=4, column=2)
 
-        tk.Label(root, text="u(s):").grid(row=4, column=0)
+        tk.Label(root, text="u(s):").grid(row=5, column=0)
         self.u_entry = tk.Entry(root)
-        self.u_entry.grid(row=4, column=1)
+        self.u_entry.grid(row=5, column=1)
 
         # Введення кількості рівнянь ПУ
-        tk.Label(root, text="Кількість рівнянь ПУ:").grid(row=5, column=0)
+        tk.Label(root, text="Кількість рівнянь ПУ:").grid(row=6, column=0)
         self.pu_count_entry = tk.Entry(root)
-        self.pu_count_entry.grid(row=5, column=1)
+        self.pu_count_entry.grid(row=6, column=1)
         self.pu_button = tk.Button(root, text="Згенерувати ПУ", command=self.generate_pu_equations)
-        self.pu_button.grid(row=5, column=2)
+        self.pu_button.grid(row=6, column=2)
 
         # Введення кількості рівнянь КУ
-        tk.Label(root, text="Кількість рівнянь КУ:").grid(row=6, column=0)
+        tk.Label(root, text="Кількість рівнянь КУ:").grid(row=7, column=0)
         self.ku_count_entry = tk.Entry(root)
-        self.ku_count_entry.grid(row=6, column=1)
+        self.ku_count_entry.grid(row=7, column=1)
         self.ku_button = tk.Button(root, text="Згенерувати КУ", command=self.generate_ku_equations)
-        self.ku_button.grid(row=6, column=2)
+        self.ku_button.grid(row=7, column=2)
 
         self.pu_frame = tk.Frame(root)
-        self.pu_frame.grid(row=7, columnspan=3)
+        self.pu_frame.grid(row=8, columnspan=3)
 
         self.ku_frame = tk.Frame(root)
-        self.ku_frame.grid(row=8, columnspan=3)
+        self.ku_frame.grid(row=9, columnspan=3)
 
         # Кнопка збереження
         self.save_button = tk.Button(root, text="Зберегти в JSON", command=self.save_to_json)
-        self.save_button.grid(row=9, column=1)
+        self.save_button.grid(row=10, column=1)
 
         # Завантаження попередніх даних
         self.load_from_json()
+
+    def solve_mode(self):
+        self.y_label.grid_forget()
+        self.y_entry.grid_forget()
+        self.calculate_button.grid_forget()
+
+    def problem_generation_mode(self):
+        self.y_label.grid(row=4, column=0)
+        self.y_entry.grid(row=4, column=1)
+        self.calculate_button.grid(row=4, column=2)
 
     def generate_pu_equations(self):
         for widget in self.pu_frame.winfo_children():
